140ca70066b79da780ec46c7c8b057ba38d019cd
avikdasm99 <110466059+avikdasm99@users.noreply.github.com>
2023-01-22 09:42:17 +0530

    Add files via upload

---
+++ Variable.py
@@ -0,0 +1,33 @@
+
+# Many Values to Multiple Variables
+a,b,c,d="Apple",12,'a',"cherry";
+print(a," ",b," ",c," ",d);
+print(a,b);
+
+
+# One Value to Multiple Variables
+v=m=n=5;    
+print(v+n+n);
+
+
+# Unpack a Collection
+fruits=["apple","Mango","Peers"];
+f,g,h=fruits;        
+print(f);print(g);print(h);
+
+z="I love Python";
+print(z);
+a1="I ";
+a2="love ";
+a3="Python";
+print(a1,a2,a3);
+print(a1+a2+a3);
+
+
+
+
+
+
+
+    
+    
