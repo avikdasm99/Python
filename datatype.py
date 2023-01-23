bf0388a6ead097d1a5af67ac1bf70746fefb149f
avikdasm99 <110466059+avikdasm99@users.noreply.github.com>
2023-01-23 13:58:21 +0530

    Add files via upload

---
+++ datatype.py
@@ -0,0 +1,33 @@
+a="pyton";                     #<class 'str'>
+print(type(a));
+
+b=4+5j;                        #<class 'complex'>
+print(type(b));
+
+c=["apple","banana","cherry"]; # <class 'list'>
+print(type(c));
+
+d=("apple","banana","cherry"); # <class 'tuple'>
+print(type(d));
+
+e=True;                        #<class 'bool'>
+print(type(e));
+
+f={"apple","banana","cherry"}; # <class 'set'>
+print(type(f));
+
+g=frozenset({"apple","banana","cherry"});   #<class 'frozenset'>
+print(type(g));
+
+h={"name":"python","rank":1};     #<class 'dict'>
+print(type(h));
+
+i=20;                            #<class 'int'>
+print(type(i));   
+
+j=30.78;                   #<class 'float'>
+print(type(j));
+
+r=range(5);                 #<class 'range'>
+print(type(r));
+
