a768771241b7d34e38e2292a840b23fa1ca4552f
avikdasm99 <110466059+avikdasm99@users.noreply.github.com>
2023-01-24 10:28:13 +0530

    Add files via upload

---
+++ String Methods.py
@@ -0,0 +1,38 @@
+# String Methods
+
+s1="avik das Python  MySQL Java";
+
+# Converts the first character to upper case
+print(s1.capitalize());
+
+# Converts string into lower case
+print(s1.casefold());
+
+# Returns a centered string
+print(s1.center(977));
+
+#Returns the number of times a specified value occurs in a string
+print(s1.count("a"));
+
+#Returns an encoded version of the string
+print(s1.encode());
+
+#Returns true if the string ends with the specified value
+print(s1.endswith("a"));
+
+#Sets the tab size of the string
+print(s1.expandtabs());
+
+#returns the position of where it was found
+print(s1.find("y"));
+
+#Returns True if all characters in the string are alphanumeric
+print(s1.isalnum());
+
+#Returns True if all characters in the string are in the alphabet
+print(s1.isalpha());
+
+
+print(s1.join(4));
+
+
